45daadee4cfe8230d6e6f540f085f47d276ded4c
FelixC <g125553693@gmail.com>
2023-06-13 16:09:28 +1000

    Update Global CSS
    - Add Debug CSS
    - Add Fa Icon Button CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HobbyTableComponent } from './hobby-table/hobby-table.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 const routes: Routes = [
-  { path: 'hobby', component: HobbyTableComponent }
+  { path: 'hobby', component: HobbyTableComponent },
+  { path: 'profile', component: MyProfileComponent }
 ];
 
 @NgModule({
